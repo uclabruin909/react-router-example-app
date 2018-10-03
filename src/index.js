be1f7bd74923ef870f3cd4f524438d709e5b0ff9
Kevin Kim <kevintaekim80@gmail.com>
2018-10-02 18:59:21 -0700

    implement basic routes with test components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+	render() {
+		return (<div>Hello</div>);
+	}
+}
+
+class Goodbye extends React.Component {
+	render() {
+		return (<div>GoodBye</div>);
+	}
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+    	<div>
+	    	<Route path="/Hello" component={Hello}></Route>
+	    	<Route path="/Goodbye" component={Goodbye}></Route>
+    	</div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container')
 );
