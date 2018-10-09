ebf54e78011feaa4daa25f5503c0340ecb61fed1
Kevin Kim <kevintaekim80@gmail.com>
2018-10-09 15:00:30 -0700

    install redux-form lib and initialize in root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducers from './reducer_posts';
 
 const rootReducer = combineReducers({
-	posts: PostsReducers
+	posts: PostsReducers,
+	form: formReducer,
 });
 
 export default rootReducer;
