1002e5803faf07189c98ae4d70e935f9b952eba5
Kevin Kim <kevintaekim80@gmail.com>
2018-10-09 14:50:19 -0700

    add mapToState and implement react-router link module

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,7 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -7,11 +9,37 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return _.map(this.props.posts, (post) => {
+			return  (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			);
+		})
+	}
+
 	render() {
 		return (
-			<div>Posts Index</div>
+			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add a Post
+					</Link>
+				</div>
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>	
+			</div>
 		);
 	}
 }
 
-export default connect(null, {fetchPosts: fetchPosts})(PostsIndex);
+function mapStateToProps(state) {
+	return {
+		posts: state.posts
+	};
+}
+
+export default connect(mapStateToProps, {fetchPosts: fetchPosts})(PostsIndex);
