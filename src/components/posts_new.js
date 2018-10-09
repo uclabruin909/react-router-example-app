b12651f67f7040cffb1b8fd16adf4289b7920e20
Kevin Kim <kevintaekim80@gmail.com>
2018-10-09 15:23:52 -0700

    update PostsNews component with redux-form field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNews extends Component {
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input type="text"
+                    className="form-control"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                PostsNews init template
-            </div>
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />   
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />                                
+            </form>
         );
     }
 }
 
-export default PostsNews;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNews);
