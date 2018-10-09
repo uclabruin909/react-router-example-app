0869f0c493b46fb0f153029c8e9cdfc635a352a2
Kevin Kim <kevintaekim80@gmail.com>
2018-10-09 14:24:16 -0700

    create PostsNew component and add route defintion

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNews extends Component {
+    render() {
+        return (
+            <div>
+                PostsNews init template
+            </div>
+        );
+    }
+}
+
+export default PostsNews;
