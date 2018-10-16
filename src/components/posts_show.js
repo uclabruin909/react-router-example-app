118f42e840a088665203700a140cbf6a802b6be0
Kevin Kim <kevintaekim80@gmail.com>
2018-10-15 23:01:21 -0700

    update PostsShow component by mapping state and rendering specific post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
+    componentDidMount() {
+        //provided by react-router
+        const postId = this.props.match.params.id;
+        this.props.fetchPost(postId);
+    }
     render() {
+        const post = this.props.post;
+        if (!post) {
+            return (
+                <div>Loading....</div>
+            );
+        }
+
         return (
             <div>
-                Posts Show!!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
 }
 
-export default PostsShow;
+function mapStateToProps(state, ownProps) {
+    return {
+        post: state.posts[ownProps.match.params.id]
+    };
+}
+
+export default connect(mapStateToProps, {fetchPost: fetchPost})(PostsShow);
